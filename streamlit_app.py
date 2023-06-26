03cc64580fb97491865f31116bec7ba74d8298d1
Bikal Thapa <bikaltarung7@gmail.com>
2023-06-26 14:16:49 -0400

    Update streamlit_app.py
    Added option to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,14 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #print the normalized response in table
 streamlit.dataframe(fruityvice_normalized)
 
+#connection to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+#query table
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding', add_my_fruit)
